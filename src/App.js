1a96b869f90ec968064e5222cec1711fef01f8c7
박현우 <gusdn3477@naver.com>
2022-03-22 17:32:57 +0900

    useEffect 실습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import InputSample from "./InputSample";
 import TextInputWithFocusButton from "./TextinputWithFocousButton";
 import UseInput from "./UseInput";
 import React, { useState, useRef, useEffect } from "react";
+import Timer from "./component/Timer";
 
 const heavyWork = () =>{
   console.log("엄청 무거운 작업!!!");
@@ -10,31 +11,89 @@ const heavyWork = () =>{
 
 function App() {
 
-  // 무거운 작업 시 콜백함수를 사용하면 lazy하게 가져와서 효율적이다.
-  const [names, setNames] = useState(() => heavyWork());
-  const [input, setInput] = useState('');
-
-  const handleInputChange = e => {
-    setInput(e.target.value);
+  const [showTimer, setShowTimer] = useState(false);
+ 
+  // 마운트 : 화면에 컴포넌트가 렌더링 되었을 때
+  // 언마운트 : 화면에 컴포넌트가 사라질 때 -> useEffect의 clean-up 기능을 이용하면 된다.
+  const handleButtonClick = async () =>{
+    setShowTimer(!showTimer)
   }
 
-  // 화살표 함수의 경우 중괄호가 있는 경우 return문을 작성해 주어야 한다.
-  const handleUpload = () => {
-    setNames(prev => 
-      {
-        console.log('이전 state: ', prev);
-        return [input, ...prev]
-      }
-    );
-  }
-
-  return (
+  return(
     <div>
-      <input type="text" value={input} onChange={handleInputChange}/>
-      <button onClick={handleUpload}>Update</button>
-      {names.map((name, idx) => <p key={idx}>{name}</p>)}
+      {showTimer && <Timer/>}
+      <button onClick={handleButtonClick}>Toggle Timer</button>
     </div>
   )
+  // const [count, setCount] = useState(1);
+  // const [name, setName] = useState('');
+
+  // const handleCountUpdate = () => {
+  //   setCount(count + 1);
+  // };
+
+  // const handleInputChange = (e) => {
+  //   setName(e.target.value);
+  // }
+
+  // // 렌더링마다 매번 실행됨 - 렌더링 이후
+  // useEffect(() => {
+  //   console.log('렌더링');
+  // });
+
+  // // 마운트 + count가 변경될 때 마다 실행
+  // useEffect(() => {
+  //   console.log('count 변화');
+  // }, [count]);
+
+  // // 마운트 + [ name ] 변경될 때 마다 실행
+  // useEffect(() => {
+  //   console.log('name 변화');
+  // }, [name]);
+
+  // // 첫 렌더링 시에만 실행됨
+  // useEffect(() => {
+  //   console.log('마운팅');
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <button onClick={handleCountUpdate}>Update</button>
+  //     <span>count: {count}</span>
+  //     <input text="text" value={name} onChange={handleInputChange}/>
+  //     <span>name: {name}</span>
+  //   </div>
+  // )
+
+  // useState 관련 자료
+  // // 무거운 작업 시 콜백함수를 사용하면 lazy하게 가져와서 효율적이다.
+  // const [names, setNames] = useState(() => heavyWork());
+  // const [input, setInput] = useState('');
+
+  // const handleInputChange = e => {
+  //   setInput(e.target.value);
+  // }
+
+  // // 화살표 함수의 경우 중괄호가 있는 경우 return문을 작성해 주어야 한다.
+  // const handleUpload = () => {
+  //   setNames(prev => 
+  //     {
+  //       console.log('이전 state: ', prev);
+  //       return [input, ...prev]
+  //     }
+  //   );
+  // }
+
+  // return (
+  //   <div>
+  //     <input type="text" value={input} onChange={handleInputChange}/>
+  //     <button onClick={handleUpload}>Update</button>
+  //     {names.map((name, idx) => <p key={idx}>{name}</p>)}
+  //   </div>
+  // )
+
+
+
   // const [count, setCount] = useState(1);
   // // const [renderCount, setRenderCount] = useState(1);
   // const renderCount = useRef(1);
